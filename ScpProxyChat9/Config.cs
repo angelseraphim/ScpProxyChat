85392e7a12b9fe37f09f2ef0f0b1ac1c0ea5eceb
AngelSeraphim <server@praniksl.ru>
2024-11-09 20:01:01 +0500

    Добавьте файлы проекта.

---
+++ ScpProxyChat9/Config.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Exiled.API.Interfaces;
+using PlayerRoles;
+
+namespace ScpProxyChat
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        public string ProxyChatEnabled { get; set; } = "<b>Proxy чат <color=green>включен</color>!</b>";
+        public string ProxyChatDisabled { get; set; } = "<b>Proxy чат <color=red>отключен</color>!</b>";
+        public float MaxDistance { get; set; } = 10f;
+        public List<RoleTypeId> AllowedRoles { get; set; } = new List<RoleTypeId>()
+        {
+            RoleTypeId.Scp049,
+            RoleTypeId.Scp096,
+            RoleTypeId.Scp106,
+            RoleTypeId.Scp173,
+            RoleTypeId.Scp0492,
+            RoleTypeId.Scp939,
+            RoleTypeId.Scp3114
+        };
+    }
+}
---
+++ ScpProxyChat9/Plugin.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using UnityEngine;
+using VoiceChat.Networking;
+using VoiceChat;
+using System.Linq;
+using System;
+
+namespace ScpProxyChat
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "angelseraphim.";
+        public override string Name => "ScpProxyChat";
+        public override string Prefix => "ScpProxyChat";
+        public override Version Version => new Version(1, 0, 0, 8);
+
+        private readonly List<Player> ToggledPlayers = new List<Player>();
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.VoiceChatting += OnVoiceChatting;
+            Exiled.Events.Handlers.Player.TogglingNoClip += OnTogglingNoClip;
+            Exiled.Events.Handlers.Player.ChangingRole += OnChangingRole;
+
+            Exiled.Events.Handlers.Server.RestartingRound += OnRestartingRound;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.VoiceChatting -= OnVoiceChatting;
+            Exiled.Events.Handlers.Player.TogglingNoClip -= OnTogglingNoClip;
+            Exiled.Events.Handlers.Player.ChangingRole -= OnChangingRole;
+
+            Exiled.Events.Handlers.Server.RestartingRound -= OnRestartingRound;
+            base.OnDisabled();
+        }
+
+        private void OnChangingRole(ChangingRoleEventArgs ev)
+        {
+            if (ToggledPlayers.Contains(ev.Player))
+                ToggledPlayers.Remove(ev.Player);
+        }
+
+        private void OnRestartingRound() => ToggledPlayers.Clear();
+
+        public void OnTogglingNoClip(TogglingNoClipEventArgs ev)
+        {
+            if (ev.Player.IsNoclipPermitted)
+                return;
+
+            if (!Config.AllowedRoles.Contains(ev.Player.Role.Type))
+                return;
+
+            if (ToggledPlayers.Contains(ev.Player))
+            {
+                ToggledPlayers.Remove(ev.Player);
+                ev.Player.ShowHint(Config.ProxyChatDisabled, 4f);
+            }
+            else
+            {
+                ToggledPlayers.Add(ev.Player);
+                ev.Player.ShowHint(Config.ProxyChatEnabled, 4f);
+            }
+        }
+
+        public void OnVoiceChatting(VoiceChattingEventArgs ev)
+        {
+            if (ev.VoiceMessage.Channel != VoiceChatChannel.ScpChat)
+                return;
+
+            if (!ToggledPlayers.Contains(ev.Player))
+                return;
+
+            VoiceMessage voiceMessage = ev.VoiceMessage;
+            foreach (Player player in Player.List.Where(p => !p.IsScp && !p.IsDead && Vector3.Distance(ev.Player.Position, p.Position) <= Config.MaxDistance))
+            {
+                voiceMessage.Channel = VoiceChatChannel.Proximity;
+                player.Connection.Send(voiceMessage);
+            }
+            foreach (Player player in ev.Player.CurrentSpectatingPlayers)
+            {
+                voiceMessage.Channel = VoiceChatChannel.Proximity;
+                player.Connection.Send(voiceMessage);
+            }
+        }
+    }
+}
